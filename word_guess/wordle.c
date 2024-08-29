b5125c2d7473e6c3d99e159e360559955a4fd86d
KartikThakur2005 <kartikthakursaklani@gmail.com>
2024-08-29 18:45:38 +0000

    adding king

--- word_guess/wordle.c
+++ word_guess/wordle.c
@@ -40,6 +40,152 @@ int readfile(char*);
 void isin(char, char*);  
 
 
+void ani()
+{
+
+           printf(ClrRed);
+           printf("+------------------------------------------------+\n");
+            printf("|                                                |\n");
+            printf("|          WELCOME TO THE Worlde GAME!           |\n");
+            printf("|                                                |\n");
+            printf("|        The SPY must caught  to save the KING   |\n");
+            printf("|                Help the King                   |\n");
+            printf("|                                                |\n");
+            printf("+------------------------------------------------+\n");
+
+            printf(ClrStop);
+        
+        
+        
+}
+
+void happy()
+{
+    sleep(1);
+    printf(ClrGreen);
+     printf("King's soldier captures the spy and starts beating him!\n");
+        printf("+------------------------------------------------+\n");
+        printf("|  KING          SOLDIER         %s%sSPY%s%s             |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|  \\o/            o             %s%so_x%s%s              |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|   |            /|\\            %s%s/|\\%s%s              |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|  / \\           / \\             %s%s/ \\%s%s             |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|                                                |\n");
+        printf("%s|%s              %s*PUNCH* *KICK*%s                    %s|\n",ClrGreen,ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|                                                |\n");
+        printf("+------------------------------------------------+\n");
+
+
+     sleep(1);
+      printf("\n\n");
+        printf("The soldier is beating the spy!\n");
+        printf("+------------------------------------------------+\n");
+        printf("|  SOLDIER      %s%s*SPY*%s%s                            |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|    o         %s%so_x%s%s                               |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|   /|\\        %s%s/|\\%s%s                               |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|   / \\        %s%s/ \\%s%s                               |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("|             %s%s*SPY IN PAIN*%s%s                      |\n",ClrStop,ClrRed,ClrStop,ClrGreen);
+        printf("+------------------------------------------------+\n");
+
+printf("\n\n");
+  sleep(1);
+        printf("+------------------------------------+\n");
+    printf("|                                    |\n");
+    printf("|     Y O U   H A V E   W O N       |\n");
+    printf("|         T H E   G A M E            |\n");
+    printf("|                                    |\n");
+    printf("+------------------------------------+\n");
+        printf(ClrStop);
+    
+}
+
+void end()
+{
+    sleep(1);
+    printf(ClrRed);
+    printf("\nSPY HAS REACHED THE KING!\n");
+            printf("+------------------------------------------------+\n");
+            printf("|  SPY ATTACKS THE KING!                          |\n");
+            printf("|  \\o/        o                                  |\n");
+            printf("|   |        /|\\      *stab*                     |\n");
+            printf("|  / \\      / \\                                  |\n");
+            printf("+------------------------------------------------+\n");
+
+    sleep(1);
+
+                printf("+------------------------------------------------+\n");
+            printf("|              KING IS DEAD                      |\n");
+            printf("|               X                                |\n");
+            printf("|              /|\\                               |\n");
+            printf("|              / \\                               |\n");
+            printf("+------------------------------------------------+\n");
+    
+    printf("\n\n");
+  sleep(1);
+    printf("+------------------------------+\n");
+    printf("|                              |\n");
+    printf("|      G A M E   O V E R       |\n");
+    printf("|                              |\n");
+    printf("+------------------------------+\n");
+
+      printf(ClrStop);
+
+
+}
+
+void midani(int rounds)
+{
+    printf("\n\n");
+    switch (rounds) {
+        case 1:
+            printf("+------------------------------------------------+\n");
+            printf("|  %sKING%s                                           |\n",ClrGreen,ClrStop);
+            printf("|  %s\\o/%s                                  %sSPY%s      |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|   %s|%s                                   %so%s        |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|  %s/ \\%s                                 /|\\       |\n",ClrGreen,ClrStop);
+            printf("|                                       %s/ \\%s      |\n",ClrRed,ClrStop);
+            printf("+------------------------------------------------+\n");
+            break;
+        case 2: 
+            printf("+------------------------------------------------+\n");
+            printf("|  %sKING%s                                           |\n",ClrGreen,ClrStop);
+            printf("|  %s\\o/%s                           %sSPY%s             |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|   %s|%s                              %so%s             |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|  %s/ \\%s                            %s/|\\%s            |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|                                  %s/ \\%s           |\n",ClrRed,ClrStop);
+            printf("+------------------------------------------------+\n");
+            break;
+        case 3:
+            printf("+------------------------------------------------+\n");
+            printf("|  %sKING%s                                           |\n",ClrGreen,ClrStop);
+            printf("|  %s\\o/%s                     %sSPY%s                   |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|   %s|%s                      %so%s                     |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|  %s/ \\%s                    %s/|\\%s                    |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|                         %s/ \\%s                    |\n",ClrRed,ClrStop);
+            printf("+------------------------------------------------+\n");
+            break;
+        case 4:
+            printf("+------------------------------------------------+\n");
+            printf("|  %sKING%s                                           |\n",ClrGreen,ClrStop);
+            printf("|  %s\\o/%s             %sSPY%s                           |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|   %s|%s              %so%s                             |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|  %s/ \\%s            %s/|\\%s                            |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|                 %s/ \\%s                            |\n",ClrRed,ClrStop);
+            printf("+------------------------------------------------+\n");
+            break;
+        case 5:
+            printf("+------------------------------------------------+\n");
+            printf("|  %sKING%s                %sSPY%s                        |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|  %s\\o/%s                %so%s                          |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|   %s|%s                %s/|\\%s                         |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("|  %s/ \\%s               %s/ \\%s                         |\n",ClrGreen,ClrStop,ClrRed,ClrStop);
+            printf("+------------------------------------------------+\n");
+            break;;
+
+            
+    }
+    printf("\n\n");
+
+}
 
 char *randomword(int m)
 {
@@ -234,15 +380,6 @@ void gameloop(char *correct)
     
 
     return ;
-    
-
-    
-    
-   
-    
-   
-     
-
 }
 
 int main(int ,char**);
@@ -267,12 +404,16 @@ int main(int argc, char *argv[])
      p = randomword(n);
      continuation = true;
 
+      ani();
+      printf("\n%s\n",p);
      while (continuation)
      {
          printf("\nROUND>%d\n", rounds);
           printf("\nEnter the word\n");
           gameloop(p);
+          midani(rounds);
           rounds++;
+          
           if (win == false && rounds >5)
         {
         
@@ -287,10 +428,13 @@ int main(int argc, char *argv[])
      if (win)
      {
          printf("\n %s Congratulation, you won the game %s\n", ClrGreen, ClrStop);
+
+         happy();
      }
      else
      {
         printf("\n %s You lost the game\n %s", ClrRed, ClrStop);
+        end();
      }
      
       
